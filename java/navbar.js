ea188d2f6411e5930ed191889041125b608111c5
antzie <39847856+antzie@users.noreply.github.com>
2018-07-25 15:38:10 +1200

    Added rest of files

---
+++ java/navbar.js
@@ -0,0 +1,10 @@
+
+function open_nav() {
+    document.getElementById("respon_nav").style.width = "200px";
+}
+
+function close_nav() {
+    document.getElementById("respon_nav").style.width = "0";
+}
+
+$('nav.ui-autocomplete').removeAttr('style');
